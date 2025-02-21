7c91b4722091ba7e0c78ef68baaa4b4a2387e0cd
Huy Nguyen <ngutahu1103@gmail.com>
2025-02-22 00:46:46 +0700

    send mail and update profile

---
+++ DataAccess/Implement/UserService.cs
@@ -0,0 +1,55 @@
+﻿using BusinessObject.Models;
+using DataAccess.Interface;
+using DataAccess.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Implement
+{
+    public class UserService : IUser
+    {
+        private Db12353Context _dbcontext = new();
+        public async Task<UserModel> GetUserDetails(int userId)
+        {
+            var result = await (from u in _dbcontext.Users
+                                join d in _dbcontext.UserDetails
+                                on u.UserId equals d.UserId into userDetail
+                                from ud in userDetail.DefaultIfEmpty()
+                                join b in _dbcontext.BankingAccounts
+                                on u.UserId equals b.UserId into bankingAccount
+                                from ba in bankingAccount.DefaultIfEmpty()
+                                join r in _dbcontext.Roles
+                                on u.RoleId equals r.RoleId
+                                where u.UserId == userId
+                                select new UserModel
+                                {
+                                    UserId = u.UserId,
+                                    RoleId = u.RoleId,
+                                    Email = u.Email,
+                                    Status = u.Status,
+                                    PhoneNumber = ud.PhoneNumber,
+                                    FullName = ud.FullName,
+                                    Account = ba.Account,
+                                    Bank = ba.Bank,
+                                    RoleName = r.RoleName,
+                                    Address = ud.Address
+                                })
+                                .AsNoTracking()
+                                .FirstOrDefaultAsync();
+            return result;
+        }
+
+        public async Task<int> UpdateUser(UserModel model)
+        {
+            //update user
+            var user = await _dbcontext.UserDetails.FirstOrDefaultAsync(x => x.UserId == model.UserId);
+            if(user is not null)
+            {
+                user.PhoneNumber = model.PhoneNumber;
+                user.FullName = model.FullName;
+                user.Address = model.Address;
+                await _dbcontext.SaveChangesAsync();
+            }
+            return user.UserId;
+        }
+    }
+}
---
+++ DataAccess/Interface/IUser.cs
@@ -0,0 +1,10 @@
+﻿using DataAccess.Model;
+
+namespace DataAccess.Interface
+{
+    public interface IUser
+    {
+        Task<UserModel> GetUserDetails(int userId);
+        Task<int> UpdateUser(UserModel model);
+    }
+}
