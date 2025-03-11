5b002f511061ec767bfdbf271f5f5463f80f94a2
Huy Nguyen <ngutahu1103@gmail.com>
2025-03-11 22:36:23 +0700

    fix change reequest

--- DataAccess/Repositories/Implement/UserRepository.cs
+++ DataAccess/Repositories/Implement/UserRepository.cs
@@ -3,6 +3,8 @@ using DataAccess.Model;
 using AutoMapper;
 using DataAccess.DAO;
 using DataAccess.Repositories.Interfaces;
+using DataAccess.DTOs.Request;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataAccess.Repositories.Implement
 {
@@ -10,10 +12,12 @@ namespace DataAccess.Repositories.Implement
     {
         private readonly UserDAO _userDAO;
         private readonly IMapper _mapper;
-        public UserRepository(UserDAO userDAO, IMapper mapper)
+        private Db12353Context _dbcontext;
+        public UserRepository(UserDAO userDAO, IMapper mapper, Db12353Context dnContext)
         {
             _userDAO = userDAO;
             _mapper = mapper;
+            _dbcontext = dnContext;
         }
 
         public async Task<UserModel> AddNewUser(UserModel userModel)
@@ -46,6 +50,56 @@ namespace DataAccess.Repositories.Implement
             {
                 return new UserModel();
             }
+        }
+
+        public async Task<UpdateUserRequest> GetUserDetails(int userId)
+        {
+            var result = await (from u in _dbcontext.Users
+                                join d in _dbcontext.UserDetails
+                                on u.UserId equals d.UserId into userDetail
+            from ud in userDetail.DefaultIfEmpty()
+                                join b in _dbcontext.BankingAccounts
+                                on u.UserId equals b.UserId into bankingAccount
+            from ba in bankingAccount.DefaultIfEmpty()
+                                join r in _dbcontext.Roles
+                                on u.RoleId equals r.RoleId
+                                where u.UserId == userId
+                                select new UpdateUserRequest
+                                {
+                                    UserId = u.UserId,
+                                    RoleId = u.RoleId,
+                                    Email = u.Email,
+                                    Status = u.Status,
+                                    Account = ba.Account,
+                                    Bank = ba.Bank,
+                                    PhoneNumber = ud.PhoneNumber,
+                                    FullName = ud.FullName,
+                                    Address = ud.Address,
+                                    RoleName = r.RoleName,
+                                    ReceiveNotification = u.ReceiveNotification
+                                })
+                                .AsNoTracking()
+                                .FirstOrDefaultAsync();
+            return result;
+        }
+
+        public async Task<int> UpdateUser(UpdateUserRequest request)
+        {
+            var u = await _dbcontext.Users.FirstOrDefaultAsync(x => x.UserId == request.UserId);
+            if (u is not null)
+            {
+                u.ReceiveNotification = request.ReceiveNotification;
+            }
+            //update user detail
+            var user = await _dbcontext.UserDetails.FirstOrDefaultAsync(x => x.UserId == request.UserId);
+            if (user is not null)
+            {
+                user.PhoneNumber = request.PhoneNumber;
+                user.FullName = request.FullName;
+                user.Address = request.Address;
+                await _dbcontext.SaveChangesAsync();
+            }
+            return user.UserId;
         }
     }
 }
--- DataAccess/Services/Implement/UserService.cs
+++ DataAccess/Services/Implement/UserService.cs
@@ -1,64 +1,26 @@
-﻿using BusinessObject.Models;
-using DataAccess.DTOs.Request;
-using DataAccess.Model;
+﻿using DataAccess.DTOs.Request;
+using DataAccess.Repositories.Interfaces;
 using DataAccess.Services.Interfaces;
-using Microsoft.EntityFrameworkCore;
 
 namespace DataAccess.Services.Implement
 {
     public class UserService : IUserService
     {
-        private Db12353Context _dbcontext = new();
+        private readonly IUserRepository _userRepository;
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
 
         // To Do
         public async Task<UpdateUserRequest> GetUserDetails(int userId)
         {
-            var result = await (from u in _dbcontext.Users
-                                join d in _dbcontext.UserDetails
-                                on u.UserId equals d.UserId into userDetail
-                                from ud in userDetail.DefaultIfEmpty()
-                                join b in _dbcontext.BankingAccounts
-                                on u.UserId equals b.UserId into bankingAccount
-                                from ba in bankingAccount.DefaultIfEmpty()
-                                join r in _dbcontext.Roles
-                                on u.RoleId equals r.RoleId
-                                where u.UserId == userId
-                                select new UpdateUserRequest
-                                {
-                                    UserId = u.UserId,
-                                    RoleId = u.RoleId,
-                                    Email = u.Email,
-                                    Status = u.Status,
-                                    Account = ba.Account,
-                                    Bank = ba.Bank,
-                                    PhoneNumber = ud.PhoneNumber,
-                                    FullName = ud.FullName,
-                                    Address = ud.Address,
-                                    RoleName = r.RoleName,
-                                    ReceiveNotification = u.ReceiveNotification
-                                })
-                                .AsNoTracking()
-                                .FirstOrDefaultAsync();
-            return result;
+            return await _userRepository.GetUserDetails(userId);
         }
 
         public async Task<int> UpdateUser(UpdateUserRequest request)
         {
-            var u = await _dbcontext.Users.FirstOrDefaultAsync(x => x.UserId == request.UserId);
-            if (u is not null)
-            {
-                u.ReceiveNotification = request.ReceiveNotification;
-            }
-            //update user detail
-            var user = await _dbcontext.UserDetails.FirstOrDefaultAsync(x => x.UserId == request.UserId);
-            if (user is not null)
-            {
-                user.PhoneNumber = request.PhoneNumber;
-                user.FullName = request.FullName;
-                user.Address = request.Address;
-                await _dbcontext.SaveChangesAsync();
-            }
-            return user.UserId;
+            return await _userRepository.UpdateUser(request);
         }
     }
 }
