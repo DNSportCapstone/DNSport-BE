856bb96ba847e93835fa94451b0d753cfc80f63a
Huy Nguyen <ngutahu1103@gmail.com>
2025-05-04 21:14:29 +0700

    update invoice ui

--- DataAccess/Repositories/Implement/BookingRepository.cs
+++ DataAccess/Repositories/Implement/BookingRepository.cs
@@ -58,11 +58,16 @@ namespace DataAccess.Repositories.Implement
         public async Task<BookingInvoiceModel> GetBookingInvoice(int bookingId)
         {
             var booking = await (from b in _dbContext.Bookings
+                                 join bf in _dbContext.BookingFields on b.BookingId equals bf.BookingId
+                                 join f in _dbContext.Fields on bf.FieldId equals f.FieldId
+                                 join s in _dbContext.Stadiums on f.StadiumId equals s.StadiumId
                                  where b.BookingId == bookingId
                                  select new BookingInvoiceModel
                                  {
                                      BookingId = b.BookingId,
                                      Date = (DateTime)b.BookingDate,
+                                     StadiumName = s.StadiumName,
+                                     StadiumAddress = s.Address,
                                      ItemBooking = (from bf in _dbContext.BookingFields
                                                     join f in _dbContext.Fields
                                                     on bf.FieldId equals f.FieldId
