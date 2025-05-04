3ecae6a6f5b352a61032fa2d0390e0762eca3cd2
SecondHand1 <choyvinhthien0209@gmail.com>
2025-05-05 00:12:00 +0700

    Fix for payment

--- DataAccess/Model/RatingModel.cs
+++ DataAccess/Model/RatingModel.cs
@@ -7,11 +7,11 @@ namespace DataAccess.Model
         public int RatingId { get; set; }
         public int BookingId { get; set; }
         public int UserId { get; set; }
-        public string FullName { get; set; }
+        public string? FullName { get; set; }
         public int? RatingValue { get; set; }
-        public string Comment { get; set; }
+        public string? Comment { get; set; }
         public DateTime? Time { get; set; }
-        public string Reply { get; set; }
+        public string? Reply { get; set; }
         public DateTime? ReplyTime { get; set; }
     }
 
