d865954752f8de1ea62e4b2661a990e04277c1bc
Bnguyen <ngobaonguyen050803@gmail.com>
2025-05-09 00:06:32 +0700

    fix voucher

--- DataAccess/DTOs/Request/CreateVoucherRequest.cs
+++ DataAccess/DTOs/Request/CreateVoucherRequest.cs
@@ -13,7 +13,7 @@ namespace DataAccess.DTOs.Request
     {
         public int? VoucherId { get; set; }
         public string VoucherCode { get; set; }
-        public int DiscountPercentage { get; set; }
+        public decimal? DiscountPercentage { get; set; }
         public DateTime ExpirationDate { get; set; }
     }
 }
